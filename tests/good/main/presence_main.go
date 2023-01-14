38548604896607bd90e9409d64e7ee6d8c99ae8f
Felix-Yvonnet <felix.yvonnet@hotmail.fr>
2023-01-14 16:38:52 +0100

    Tests for print

--- tests/good/main/presence_main.go
+++ tests/good/main/presence_main.go
@@ -4,5 +4,4 @@ func main(){};
 
 /*
 == Expected compiler output ==
-
 */
---
+++ tests/good/printf/print_int.go
@@ -0,0 +1,11 @@
+package main;
+import "fmt";
+
+func main(){
+	fmt.Print(12,13)
+}
+
+/*
+== Expected program output ==
+12 13
+*/
