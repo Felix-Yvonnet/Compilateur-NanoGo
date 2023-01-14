fe3d7610acc6e7202546b2fb7b97490f8e0d9ceb
Felix-Yvonnet <felix.yvonnet@hotmail.fr>
2023-01-14 18:40:43 +0100

    Added tests for if

---
+++ tests/good/if/ifathenb.go
@@ -0,0 +1,16 @@
+package main;
+import "fmt";
+
+func main(){
+
+	if 3>2 {
+		fmt.Print("3>2")
+	} else {
+		fmt.Print("What were you expecting ?")
+	}
+}
+
+/*
+== Expected program output ==
+3>2
+*/
