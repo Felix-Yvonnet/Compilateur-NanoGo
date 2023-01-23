9c215d7fa2efc8484b834d91b30c5c1cebeb9252
Felix-Yvonnet <felix.yvonnet@hotmail.fr>
2023-01-23 17:23:54 +0100

    Corrected evaluation order and added simple functions

--- compile.ml
+++ compile.ml
@@ -24,6 +24,17 @@
 
 *)
 
+
+
+(*
+   rsi ; src
+   rdi ; dest
+   rcx ; len
+   call memcpy
+*)
+
+
+
 open Format
 open Ast
 open Tast
@@ -42,8 +53,8 @@ let alloc_string =
     Hashtbl.add strings l s;
     l
 
-let malloc n = movq (imm n) (reg rdi) ++ call "malloc"
-let allocz n = movq (imm n) (reg rdi) ++ call "allocz"
+let malloc n = movq (imm n) !%rdi ++ call "malloc"
+let allocz n = movq (imm n) !%rdi ++ call "allocz"
 
 let sizeof = Typing.sizeof
 
@@ -51,21 +62,19 @@ let new_label =
   let r = ref 0 in fun () -> incr r; "L_" ^ string_of_int !r
 
 type env = {
-  exit_label: string;
-  ofs_this: int;
-  nb_locals: int ref; (* maximum *)
-  next_local: int; (* 0, 1, ... *)
+  mutable ofs_this: int;
+  mutable next_local: int; (* maximum *)
   local_var: (string, int) Hashtbl.t;
 }
 
 let empty_env =
-  { exit_label = ""; ofs_this = -1; nb_locals = ref 0; next_local = 0; local_var = Hashtbl.create 10 }
+  { ofs_this = -1; next_local = 0; local_var = Hashtbl.create 10 }
 
 let rec print_type ty = match ty with
   | Tint |Tptr _ | Twild-> call "print_int"
   | Tbool -> let l_false = new_label () and l_true = new_label () in testq !%rdi !%rdi ++ jz l_false ++ 
-            movq (ilab (alloc_string "true")) (reg rdi) ++ jmp l_true ++ label l_false ++
-            movq (ilab (alloc_string "false")) (reg rdi) ++
+            movq (ilab "S_true") !%rdi ++ jmp l_true ++ label l_false ++
+            movq (ilab "S_false") !%rdi ++
             label l_true ++ call "print_str"
   | Tstring -> call "print_str"
   | Tstruct {s_name;s_fields} -> movq (ilab "S_lbracket") !%rdi ++ Hashtbl.fold  (fun z y x -> x ++ 
@@ -79,20 +88,25 @@ let rec get_offset env = function
   | TEunop (Ustar, x) -> get_offset env x.expr_desc
   | _ -> failwith "Not a l-value."
 
+let rec move_return_value added_ofs base_ofs = function
+  | [] -> nop
+  | t::q ->  move_return_value (added_ofs+sizeof t.expr_typ) base_ofs q ++
+             popq rdi ++ movq !%rdi (ind ~ofs: (added_ofs+base_ofs) rbp)
+
 let rec expr env e = match e.expr_desc with
   | TEskip ->
     nop
   | TEconstant (Cbool true) ->
-    movq (imm 1) (reg rdi)
+    movq (imm 1) !%rdi
   | TEconstant (Cbool false) ->
-    movq (imm 0) (reg rdi)
+    movq (imm 0) !%rdi
   | TEconstant (Cint x) ->
-    movq (imm64 x) (reg rdi)
+    movq (imm64 x) !%rdi
   | TEnil ->
-    xorq (reg rdi) (reg rdi)
+    xorq !%rdi !%rdi
   | TEconstant (Cstring s) ->
     let l = alloc_string s in
-    movq (ilab l) (reg rdi) (* ++ call "strdup" *)
+    movq (ilab l) !%rdi
   | TEbinop (Band, e1, e2) ->
     let l_end = new_label () in
     expr env e1 ++
@@ -116,12 +130,12 @@ let rec expr env e = match e.expr_desc with
       | _ -> failwith "Stop bothering me VS code\n"
     in
     let l_true = new_label() and l_end = new_label() in
-    expr env e1 ++ pushq (reg rdi) ++
+    expr env e1 ++ pushq !%rdi ++
     expr env e2 ++ popq rsi ++
-    cmpq (reg rdi) (reg rsi) ++
+    cmpq !%rdi !%rsi ++
     j_act l_true ++
-    movq (imm 0) (reg rdi) ++ jmp l_end ++
-    label l_true ++ movq (imm 1) (reg rdi) ++
+    movq (imm 0) !%rdi ++ jmp l_end ++
+    label l_true ++ movq (imm 1) !%rdi ++
     label l_end
   | TEbinop (Badd | Bsub | Bmul as op, e1, e2) ->
     let act = match op with
@@ -130,23 +144,23 @@ let rec expr env e = match e.expr_desc with
       | Bmul -> imulq
       | _ -> failwith "Stop bothering me VS code\n"
     in
-    expr env e2 ++ pushq (reg rdi) ++
+    expr env e2 ++ pushq !%rdi ++
     expr env e1 ++ popq r12 ++
-    act (reg r12) (reg rdi)
+    act !%r12 !%rdi
   | TEbinop (Bdiv, e1, e2) ->
-    expr env e1 ++ pushq (reg rdi) ++
+    expr env e1 ++ pushq !%rdi ++
     expr env e2 ++ popq rax ++
-    xorq (reg rdx) (reg rdx) ++
+    xorq !%rdx !%rdx ++
     cqto ++
-    idivq (reg rdi) ++
+    idivq !%rdi ++
     movq !%rax !%rdi
   | TEbinop (Bmod, e1, e2) ->
-    expr env e1 ++ pushq (reg rdi) ++
+    expr env e1 ++ pushq !%rdi ++
     expr env e2 ++ popq rax ++
-    xorq (reg rdx) (reg rdx) ++
+    xorq !%rdx !%rdx ++
     cqto ++
-    idivq (reg rdi) ++
-    movq (reg rdx) (reg rdi)
+    idivq !%rdi ++
+    movq !%rdx !%rdi
   | TEbinop (Beq | Bne as op, e1, e2) ->
     let j_act = match op with
       | Beq -> jz
@@ -154,16 +168,16 @@ let rec expr env e = match e.expr_desc with
       | _ -> failwith "Stop bothering me VS code\n"
     in
     let l_true = new_label() and l_end = new_label() in
-    expr env e1 ++ pushq (reg rdi) ++
+    expr env e1 ++ pushq !%rdi ++
     expr env e2 ++ popq rsi ++
     (if e1.expr_typ = Tstring then call "strcmp" ++ testq !%rax !%rax
-    else cmpq (reg rdi) (reg rsi)) ++ j_act l_true ++
-    movq (imm 0) (reg rdi) ++ jmp l_end ++
-    label l_true ++ movq (imm 1) (reg rdi) ++
+    else cmpq !%rdi !%rsi) ++ j_act l_true ++
+    movq (imm 0) !%rdi ++ jmp l_end ++
+    label l_true ++ movq (imm 1) !%rdi ++
     label l_end
   | TEunop (Uneg, e1) ->
     expr env e1 ++
-    negq (reg rdi)
+    negq !%rdi
   | TEunop (Unot, e1) ->
     let l_un = new_label () and l_zero = new_label () in
     expr env e1 ++
@@ -176,19 +190,14 @@ let rec expr env e = match e.expr_desc with
     label l_zero
 
   | TEunop (Uamp, e1) ->
-    expr env e1
+    movq !%rbp !%rdi ++
+    subq (imm (get_offset env e1.expr_desc)) !%rdi
   | TEunop (Ustar, e1) ->
     expr env e1 ++
     movq (ind rdi) !%rdi
   | TEprint el ->
-    let rec aux = function
-      | [] -> nop
-      | t::q -> expr env t ++ print_type t.expr_typ ++ 
-                 (if q!=[] && t.expr_typ != Tstring then movq (ilab "S_space") (reg rdi) ++ call "print_str" else nop ) ++ aux q
-    in
-    aux el ++
-    movq (ilab "S_newline") (reg rdi) ++
-    call "print_str"
+    List.fold_left (fun x y -> expr env y ++ pushq !%rdi ++ x) nop el ++
+    List.fold_left (fun x y -> x ++ popq rdi ++ print_type y.expr_typ) nop el
 
   | TEident x ->
     (* For debugging :
@@ -213,15 +222,17 @@ let rec expr env e = match e.expr_desc with
       | t::q -> begin
           match t.expr_desc with 
           | TEvars (vl,eel) -> 
+            let vl = List.rev vl and eel = List.rev eel in 
             let max_size = List.fold_left (fun x y -> x + sizeof y.v_typ) 0 vl in
             let k = ref (sizeof (List.hd (List.rev vl)).v_typ) in
-            let f c = let a = Hashtbl.add env.local_var c.v_name (!(env.nb_locals) + max_size - !k) in 
+            let f c = let a = Hashtbl.add env.local_var c.v_name (env.next_local + max_size - !k) in 
                       k := !k + sizeof c.v_typ; a in
             List.iter f vl;
-            env.nb_locals := !(env.nb_locals) + max_size;
+            env.next_local <- env.next_local + max_size;
             List.fold_left (fun c e -> expr env e ++ pushq !%rdi ++ c) nop eel ++
+
             aux q
-          | _ -> let code = expr env t in code ++ aux q
+          | _ -> let code2 = expr env t in code2 ++ aux q
     end
     in aux el in code ++
     let rec aux = function
@@ -229,7 +240,7 @@ let rec expr env e = match e.expr_desc with
     | t::q -> begin 
         match t.expr_desc with 
         | TEvars (vl,eel) -> 
-          let f c = env.nb_locals:= !(env.nb_locals) - sizeof c.v_typ; Hashtbl.remove env.local_var c.v_name in
+          let f c = env.next_local <- env.next_local - sizeof c.v_typ; Hashtbl.remove env.local_var c.v_name in
           List.iter f vl;
           List.fold_left (fun c e -> popq rdi ++ c) nop eel ++
           aux q
@@ -240,7 +251,7 @@ let rec expr env e = match e.expr_desc with
   | TEif (e1, e2, e3) ->
     let l_false = new_label() and l_end = new_label() in
     expr env e1 ++
-    testq (reg rdi) (reg rdi) ++
+    testq !%rdi !%rdi ++
     jz l_false ++
     expr env e2 ++
     jmp l_end ++
@@ -258,25 +269,33 @@ let rec expr env e = match e.expr_desc with
      label l_end
   | TEnew ty ->
      allocz (sizeof ty) ++
-     movq (reg rax) (reg rdi)
+     movq !%rax !%rdi
   | TEcall (f, el) ->
     let n = List.length el in
-    List.fold_left (fun c t -> c ++ subq (imm (sizeof t)) !%rsp ) nop f.fn_typ ++ (* on descend pour laisser la place aux rez *)
-    List.fold_left (fun c e -> c ++ expr env e ++ pushq !%rdi) nop el ++ (* on ajoute les arguments évalués *)
+    (if List.length f.fn_typ > 1 then
+    List.fold_left (fun c t -> c ++ subq (imm (sizeof t)) !%rsp ) nop f.fn_typ (* on descend pour laisser la place aux rez *)
+    else nop ) ++
+    List.fold_left (fun c e -> expr env e ++ pushq !%rdi ++  c) nop el ++ (* on ajoute les arguments évalués *)
     call ("F_"^f.fn_name) ++
-    if e.expr_typ != Twild then addq (imm (8*n)) !%rsp ++ popq rax else nop
+    addq (imm (8*n)) !%rsp
 
   | TEdot (e1, {f_ofs=ofs}) ->
      (* TODO code pour e.f *) assert false
   | TEvars _ ->
      assert false (* fait dans block *)
   | TEreturn [] ->
+    movq !%rbp !%rsp ++
+    popq rbp ++
     ret
   | TEreturn [e1] ->
     expr env e1 ++
+    movq !%rbp !%rsp ++
+    popq rbp ++
     ret
   | TEreturn el ->
-    List.fold_left (fun c e -> c ++ expr env e ++ pushq !%rdi) nop el ++
+    move_return_value 0 env.ofs_this el ++
+    movq !%rbp !%rsi ++
+    popq rbp ++
     ret
   | TEincdec (e1, op) ->
     let act = match op with
@@ -288,11 +307,15 @@ let rec expr env e = match e.expr_desc with
     act !%rdi ++
     movq !%rdi (ind ~ofs:ofs rbp)
 
-
 let function_ f e =
   if !debug then eprintf "function %s:@." f.fn_name;
   let s = f.fn_name in
-  label ("F_" ^ s) ++ pushq !%rbp ++ movq !%rsp !%rbp ++ expr empty_env e ++ popq rbp ++ ret
+  let env = empty_env in
+  env.next_local <- -24;
+  List.iter (fun x -> Hashtbl.add env.local_var x.v_name env.next_local; env.next_local <- env.next_local + sizeof x.v_typ) f.fn_params;
+  env.ofs_this <- List.fold_left (fun x y -> x + sizeof y.v_typ) 0 f.fn_params;
+  env.next_local <- 0;
+  label ("F_" ^ s) ++ pushq !%rbp ++ movq !%rsp !%rbp ++ expr env e ++ popq rbp ++ ret
 
 let decl code = function
   | TDfunction (f, e) -> code ++ function_ f e
@@ -305,7 +328,7 @@ let file ?debug:(b=false) dl =
   { text =
       globl "main" ++ label "main" ++
       call "F_main" ++
-      xorq (reg rax) (reg rax) ++
+      xorq !%rax !%rax ++
       ret ++
       funs ++
       inline "
@@ -331,6 +354,8 @@ print_str:
       label "S_newline" ++ string "\n" ++
       label "S_lbracket" ++ string "{" ++      
       label "S_rbracket" ++ string "}" ++
+      label "S_true" ++ string "true" ++
+      label "S_false" ++ string "false" ++
       (Hashtbl.fold (fun l s d -> label l ++ string s ++ d) strings nop)
     ;
   }
